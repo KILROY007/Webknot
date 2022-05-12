9637de613b1122f306e8eef37629946cd48e907d
KILROY007 <sumanthegde123@gmail.com>
2022-05-12 19:13:53 +0530

    webknot3

--- src/sharedFolder/Dropdown.js
+++ src/sharedFolder/Dropdown.js
@@ -8,10 +8,10 @@ export default function DropDown(props){
                 {props.name}
         </label>
         <div className="relative ">
-            <select className={`block appearance-none w-full  border border-gray-300 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-textFieldOutliner-primary mt-1`} id="grid-state" name={`${props.name}`}>
-            <option>{props.option1}</option>
-            <option>{props.option2}</option>
-            <option>{props.option3}</option>
+            <select className={` w-full  border border-gray-300 text-black py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-textFieldOutliner-primary mt-1`} id="grid-state" name={`${props.name}`}>
+                <option>{props.option1}</option>
+                <option>{props.option2}</option>
+                <option>{props.option3}</option>
             </select>
         </div>
     </div>
