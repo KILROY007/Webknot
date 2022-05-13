ac4714317f89ec491e0d66f771f8731f4248b132
KILROY007 <sumanthegde123@gmail.com>
2022-05-13 16:19:48 +0530

    search

--- src/sharedFolder/desktop/navbar/DesNavbar.js
+++ src/sharedFolder/desktop/navbar/DesNavbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import {GoSearch} from 'react-icons/go'
-
+import {Input} from '../../Input'
 export const DesNavbar = (props) => {
   return (
-    <nav id="DesNavbar">
-    <div className="flex justify-between my-4 mx-4">
+    <nav id="DesNavbar ">
+    <div className="flex justify-between my-4 mx-4 ">
         <div className="flex ">
             <img src={props.logo} alt="" className={`h-12`}></img>
             <h1 className={`text-3xl tracking-widest ${props.logoTextStyle} ${props.logoTextColor} mt-2`}>{props.logoText}</h1>
             <span className={`text-2xl ${props.searchColor} mt-4 ml-20`}><GoSearch/></span> 
-            <span className={`${props.searchColor} text-large mt-4 ml-2`}>Search..</span>
+            <span className={`text-large -mt-5 -ml-3 `}><Input placeholder="Search.."/></span>
         </div>
         <div> 
             <img src={props.profilePic} className={`h-12 rounded-3xl mr-8`} alt=""></img>
