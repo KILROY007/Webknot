77fd8c2725bce9e460f5ce9c0151b7f6b75bcd58
KILROY007 <sumanthegde123@gmail.com>
2022-05-13 16:38:53 +0530

    mobileNavbar

--- src/sharedFolder/mobile/drawer/MuiDrawer.js
+++ src/sharedFolder/mobile/drawer/MuiDrawer.js
@@ -5,7 +5,7 @@ import MenuIcon from '@mui/icons-material/Menu'
 import {ImStatsBars} from 'react-icons/im'
 import {GiNotebook} from 'react-icons/gi'
 import {RiPenNibFill} from 'react-icons/ri'
-
+import './drawer.css'
 
 export const MuiDrawer = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false)
@@ -17,7 +17,8 @@ export const MuiDrawer = () => {
         size='large'
         edge='start'
         color='inherit'
-        aria-label='logo'>
+        aria-label='logo'
+        padding-top="0">
         <MenuIcon  />
       </IconButton>
       <Drawer
@@ -25,20 +26,23 @@ export const MuiDrawer = () => {
         open={isDrawerOpen}
         onClose={() => setIsDrawerOpen(false)}
         >
-        <Box p={2} width='250px' role='presentation' textAlign='left'>
-          <div className="sideBar">
+        <Box p={2} width='250px' role='presentation' textAlign='left' backgroundColor="#1c2237" height="100vh">
+          <div className="sideBar text-white">
             <div className="text-4xl font-semibold">Menu</div>
             <div className="flex gap-4 mb-8 mt-8">
-              <span className="mt-1 text-2xl"><ImStatsBars/></span>
-              <button className="text-2xl font-medium" onClick={()=>window.location.pathname=''}>Dashboard</button> 
+              <span className="mt-1 text-3xl"><ImStatsBars/></span>
+              <button className="text-2xl font-medium" 
+              id={window.location.pathname==='/' ? "menuList" : ""} onClick={()=>window.location.pathname=''}>Dashboard</button> 
             </div>
             <div className="flex gap-4 mb-8" >
-              <span className="mt-1 text-2xl"><GiNotebook/></span>
-              <button className="text-2xl font-medium" onClick={()=>window.location.pathname='/issues'}>Issues</button>
+              <span className="mt-1 text-3xl"><GiNotebook/></span>
+              <button className="text-2xl font-medium" 
+              id={window.location.pathname==='/issues' ? "menuList" : ""} onClick={()=>window.location.pathname='/issues'}>Issues</button>
             </div>
             <div className="flex gap-4">
-              <span className="mt-1 text-2xl"><RiPenNibFill/></span>
-              <button className="text-2xl font-medium" onClick={()=>window.location.pathname='/create'}>Create</button>
+              <span className="mt-1 text-3xl"><RiPenNibFill/></span>
+              <button className="text-2xl font-medium" 
+              id={window.location.pathname==='/create' ? "menuList" : ""} onClick={()=>window.location.pathname='/create'}>Create</button>
             </div>
           </div>
         </Box>
